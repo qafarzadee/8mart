562b2bb58cd64ddc60238b31e6d8cd2be0425148
Elgun Gafarzade <elgungafarzade@Elgun-MacBook-Air.local>
2023-03-09 03:03:47 +0400

    book

---
+++ 8mart/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+namespace _8mart
+{
+	public class Book
+	{
+		public Book()
+		{
+
+
+
+			public string Name { get; set; }
+
+		public int PageCount { get; set; }
+
+
+		public Book(string name, int pageCount)
+		{
+			Name = name;
+			PageCount = pageCount;
+		}
+
+
+
+	}
+
+
+
+		
+	}
+}
+
+
---
+++ 8mart/Program.cs
@@ -0,0 +1,75 @@
+﻿
+
+using _8mart;
+
+Book[] Books = new Book[2];
+
+for (int i = 0; i < Books.Length; i++)
+
+{
+    Console.WriteLine("kitab adi :  );
+        string name = Console.ReadLine();
+    Console.WriteLine("kitab seyife sayi");
+    int page=int.Parse(Console.ReadLine());
+
+
+
+
+    while (!CheckName(name))
+    {
+        Console.WriteLine("adi duzgun daxil et ");
+         name = Console.ReadLine();
+
+
+    }
+
+    while (!CheckCount(page))
+    {
+        Console.WriteLine("page duzgun daxil et");
+        page = int.Parse(Console.ReadLine());
+
+
+
+    }
+
+
+
+
+    Book book = new Book(name, page);
+
+    Books[i] = book;
+
+
+}
+
+
+bool CheckName(string name)
+{
+
+    if (!string.IsNullOrWhiteSpace(name) && name.Length > 2)
+        return true;
+
+    return false;
+
+}
+
+
+bool CheckCount(int pageCount)
+{
+
+    if (pageCount>2)
+        return true;
+
+    return false;
+
+}
+
+
+foreach (Book book in Books)
+{
+
+    Console.WriteLine($"Name : {book.Name}  pageCount : {book.PageCount}");
+
+
+}
+
